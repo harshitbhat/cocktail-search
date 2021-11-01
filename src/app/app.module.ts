3a617dbb516cc089e411666525539b9836fcccab
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-11-01 21:39:35 +0530

    getting results from api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,16 +8,9 @@ import { FormComponent } from './components/form/form.component';
 import { ResultsComponent } from './components/results/results.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FormComponent,
-    ResultsComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, FormComponent, ResultsComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/services/cocktail.service.ts
+++ src/app/services/cocktail.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CocktailService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  public search(query: string) {
+    return this.http.get(
+      'https://www.thecocktaildb.com/api/json/v1/1/search.php',
+      {
+        params: {
+          s: query,
+        },
+      }
+    );
+  }
 }
