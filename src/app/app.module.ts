36de865107fd7e64325d07cc6906a24cf550d361
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-11-01 19:18:36 +0530

    Add bootstrap and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './components/form/form.component';
+import { ResultsComponent } from './components/results/results.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    ResultsComponent
   ],
   imports: [
     BrowserModule,
