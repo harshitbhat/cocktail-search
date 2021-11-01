6b8c4fe0e6ada904627952d2c04a893332966e97
Harshit Bhat <harshitmcmxcv@gmail.com>
2021-11-01 20:19:41 +0530

    getting query string

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  styleUrls: ['./form.component.css'],
 })
 export class FormComponent implements OnInit {
+  query = '';
+  @Output() submitted = new EventEmitter<string>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  getQueryString(event: Event) {
+    const target = event.target as HTMLInputElement;
+    this.query = target.value;
   }
 
+  onSubmit(event: Event) {
+    //event.stopPropagation();
+    event.preventDefault();
+    this.submitted.emit(this.query);
+  }
 }
